5698819c5e3b9617b1ddf02c7f27ce48e045c6db
Helga Þöll Guðjónsdóttir <32647181+hthg13@users.noreply.github.com>
2020-02-20 18:57:44 +0000

    Add files via upload

---
+++ spadar.js
@@ -0,0 +1,181 @@
+/////////////////////////////////////////////////////////////////
+//    Byggt á sýnidæmi um sólkerfi
+/////////////////////////////////////////////////////////////////
+var canvas;
+var gl;
+
+var numVertices  = 36;
+
+var points = [];
+var colors = [];
+
+var movement = false;     // Do we rotate?
+var spinX = 0;
+var spinY = 0;
+var origX;
+var origY;
+
+var rotOne = 0.0;
+
+var rotTwo = 0.0;
+
+var matrixLoc;
+
+
+window.onload = function init()
+{
+    canvas = document.getElementById( "gl-canvas" );
+
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+
+    colorCube();
+
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 0, 0, 0, 1.0 );
+
+    gl.enable(gl.DEPTH_TEST);
+
+    //
+    //  Load shaders and initialize attribute buffers
+    //
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+
+    var cBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW );
+
+    var vColor = gl.getAttribLocation( program, "vColor" );
+    gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vColor );
+
+    var vBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
+
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+
+    matrixLoc = gl.getUniformLocation( program, "rotation" );
+
+    //event listeners for mouse
+    canvas.addEventListener("mousedown", function(e){
+        movement = true;
+        origX = e.offsetX;
+        origY = e.offsetY;
+        e.preventDefault();         // Disable drag and drop
+    } );
+
+    canvas.addEventListener("mouseup", function(e){
+        movement = false;
+    } );
+
+    canvas.addEventListener("mousemove", function(e){
+        if(movement) {
+    	    spinY = ( spinY + (e.offsetX - origX) ) % 360;
+            spinX = ( spinX + (e.offsetY - origY) ) % 360;
+            origX = e.offsetX;
+            origY = e.offsetY;
+        }
+    } );
+
+    render();
+}
+
+function colorCube()
+{
+    quad( 1, 0, 3, 2 );
+    quad( 2, 3, 7, 6 );
+    quad( 3, 0, 4, 7 );
+    quad( 6, 5, 1, 2 );
+    quad( 4, 5, 6, 7 );
+    quad( 5, 4, 0, 1 );
+}
+
+function quad(a, b, c, d) 
+{
+    var vertices = [
+        vec3( -0.5, -0.5,  0.5 ),
+        vec3( -0.5,  0.5,  0.5 ),
+        vec3(  0.5,  0.5,  0.5 ),
+        vec3(  0.5, -0.5,  0.5 ),
+        vec3( -0.5, -0.5, -0.5 ),
+        vec3( -0.5,  0.5, -0.5 ),
+        vec3(  0.5,  0.5, -0.5 ),
+        vec3(  0.5, -0.5, -0.5 )
+    ];
+
+    var vertexColors = [
+        [ 0.0, 0.0, 0.0, 1.0 ],  // black
+        [ 1.0, 0.0, 0.0, 1.0 ],  // red
+        [ 1.0, 1.0, 0.0, 1.0 ],  // yellow
+        [ 0.0, 1.0, 0.0, 1.0 ],  // green
+        [ 0.0, 0.0, 1.0, 1.0 ],  // blue
+        [ 1.0, 0.0, 1.0, 1.0 ],  // magenta
+        [ 0.0, 1.0, 1.0, 1.0 ],  // cyan
+        [ 1.0, 1.0, 1.0, 1.0 ]   // white
+    ];
+
+    // We need to parition the quad into two triangles in order for
+    // WebGL to be able to render it.  In this case, we create two
+    // triangles from the quad indices
+    
+    //vertex color assigned by the index of the vertex
+    
+    var indices = [ a, b, c, a, c, d ];
+
+    for ( var i = 0; i < indices.length; ++i ) {
+        points.push( vertices[indices[i]] );
+        colors.push(vertexColors[a]);
+        
+    }
+}
+
+/**
+ * Býr til teninga af gefnum stærðum kvarðar og skalar.
+ * @param {translate x} tx 
+ * @param {translate y} ty 
+ * @param {translate z} tz 
+ * @param {scale x} sx 
+ * @param {scale y} sy 
+ * @param {scale z} sz 
+ * @param {mat4} mv 
+ */
+function build(tx,ty,tz,sx,sy,sz,rotation,mv) {
+    mv = mult( mv, translate(tx, ty, tz));
+    mv1 = mult( mv, rotateY(rotation));
+    mv2 = mult( mv1, scalem(sx, sy, sz));
+
+    return mv2; 
+}
+
+
+function render()
+{
+    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    rotOne += 1.0;
+    rotTwo += -1.0;
+
+    var mv = mat4();
+    mv = mult( mv, rotateX(spinX) );
+    mv = mult( mv, rotateY(spinY) );
+
+    var mv2 = mat4();
+    mv2 = mult( mv2, rotateX(spinX) );
+    mv2 = mult( mv2, rotateY(spinY) );
+
+    // teikna One
+    mv = build(0,0.05,0, 0.1,0.1,0.9,rotOne,mv);
+    gl.uniformMatrix4fv(matrixLoc, false, flatten(mv));
+    gl.drawArrays( gl.TRIANGLES, 0, numVertices );
+
+    // Teiknum Two
+    mv2 = build(0,-0.05,0, 0.1,0.1,0.9,rotTwo,mv2);
+    gl.uniformMatrix4fv(matrixLoc, false, flatten(mv2));
+    gl.drawArrays( gl.TRIANGLES, 0, numVertices );
+
+    requestAnimFrame( render );
+}
